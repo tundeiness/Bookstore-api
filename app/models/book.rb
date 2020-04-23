d3d3ce59cb2ec1e9203137d20a7023322517be99
tundeiness <tundeiness@yahoo.co.uk>
2020-04-23 13:39:54 +0100

    add Rack reducer in Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,13 @@
 class Book < ApplicationRecord
   validates :title, :category, presence: true
 
-  scope :categorized, -> (category) { where category: category }
+  # scope :categorized, -> (category) { where category: category }
+
+  # Step 1: Instantiate a reducer
+  BookReducer = Rack::Reducer.new(
+    Book.all,
+    ->(category:) { where(category: category) },
+  )
+
+
 end
